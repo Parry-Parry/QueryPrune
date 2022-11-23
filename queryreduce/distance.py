056ad09b49a46f25c60c7eed087c65f8c3c470fd
Andrew <2393265p@student.gla.ac.uk>
2022-11-23 17:04:23 +0000

    Completing faiss work for markov process, replaced array with dict

--- queryreduce/distance.py
+++ queryreduce/distance.py
@@ -1,7 +1,9 @@
 from typing import Tuple
 import faiss
+from faiss.loader import 
 from functools import partial
 import numpy as np 
+
 
 '''
 Distance functions for Markov Process sample choice
@@ -23,6 +25,26 @@ def init_interpolated_distance(resource, alpha, beta, equal=False):
     norm = partial(faiss.pairwise_distance_gpu, resource)
     def distance(x, xs):
         return alpha * norm(x[:, 0], xs[:, 0]) + beta * norm(x[:, 1], xs[:, 1]) + gamma * norm(x[:, 2], xs[:, 2])
+
+    return distance
+
+def init_interpolated_similarity(resource, alpha, beta, equal=False, METRIC=None):
+    if not METRIC:
+        METRIC = faiss.METRIC_INNER_PRODUCT
+
+    gamma = 1 - alpha - beta
+    if gamma < 0: gamma = 0
+    if equal:
+        alpha, beta, gamma = 1, 1, 1
+
+    norm = lambda x : faiss.normalize_L2(x)
+    dist = lambda x, y : faiss.pairwise_distance_gpu(resource, norm(x), norm(y), metric=METRIC) 
+
+    def distance(x, xs):
+        q = alpha * dist(x[:, 0], xs[:, 0])
+        pos = beta * dist(x[:, 1], xs[:, 1])
+        neg = gamma * dist(x[:, 2], xs[:, 2])
+        return q + pos + neg
 
     return distance
 
--- queryreduce/hmm/model.py
+++ queryreduce/hmm/model.py
@@ -1,8 +1,9 @@
+from collections import defaultdict
+from typing import Dict
 import numpy as np
-from queryreduce.distance import init_interpolated_distance 
+from queryreduce.distance import init_interpolated_similarity
 from queryreduce.config import MarkovConfig
-import torch 
-from functools import partial
+import faiss
 
 class Process:
     '''
@@ -13,11 +14,11 @@ class Process:
     alpha : float -> Weight of query embedding on distance 
     beta : float -> Weight of positive document on distance
     triples : np.array -> Set of embeddings of shape [num_samples, num_embeddings, embed_dim]
-    distr : torch.distribution -> Some categorical distribution
+    *Not Used Currently* distr : torch.distribution -> Some categorical distribution
 
     Generated Parameters
     --------------------
-    P : np.array -> Generate from the number of triples, represents the transition probability matrix 
+    P : dict[np.array] -> Represents the transition probability matrix 
     distance : func -> Function to calculate a pairwise distance across queries and documents
 
     run() Parameters
@@ -28,20 +29,21 @@ class Process:
 
     state_id = 0
     def __init__(self, config : MarkovConfig) -> None:
-        self.P = np.zeros((config.triple.shapes[0], config.triple.shapes[0]), dtype=np.float16)
-        self.distance = init_interpolated_distance(config.gpu, config.alpha, config.beta)
         self.triples = config.triples
-        self.sample_distr = config.distr
+        self.P : Dict[int, np.array] = defaultdict(lambda : np.zeros(self.triples.shape[0]))
+        self.distance = init_interpolated_similarity(config.gpu, config.alpha, config.beta, config.equal)
+    
+        #self.sample_distr = config.distr
 
     def _weight(self, x, xs):
-        return np.exp(self.distance(x, xs)**2)
+        return np.exp(-(self.distance(x, xs)**2))
     
     def _step(self):
         if np.all(self.P[self.state_id] == 0):
             self.P[self.state_id] = self._weight(np.expand_dims(self.triples[self.state_id], axis=0), self.triples)
+            faiss.normalize_L2(self.P[self.state_id])
         
-        distr = self.sample_distr(logits=self.P[self.state_id])
-        self.state_id = distr.sample()
+        self.state_id = np.random.choice(self.P[self.state_id], 1, p=self.P[self.state_id])
 
         return self.state_id
     
