bc2ae4d7b52428b6f75673012d654d4bbd68e06e
Andrew <2393265p@student.gla.ac.uk>
2022-11-25 14:21:43 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='queryreduce',
+    version='0.1.0',
+    author='Andrew Parry',
+    author_email='a.parry.1@research.gla.ac.uk',
+    description="Reduction of Dataset by Markov Heuristic Search",
+    url='https://github.com/Parry-Parry/QueryReduce',
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+)
