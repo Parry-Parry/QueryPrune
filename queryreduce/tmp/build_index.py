5fcea69ef5f343dff2264518663ea6e1afd749c8
Andrew Parry <2393265p@student.gla.ac.uk>
2022-12-02 16:09:15 +0000

    Changed metric behavious

--- queryreduce/tmp/build_index.py
+++ queryreduce/tmp/build_index.py
@@ -11,6 +11,7 @@ parser.add_argument('-source', type=str)
 parser.add_argument('-k', type=int)
 parser.add_argument('-out', type=str) 
 parser.add_argument('--compress', action="store_true")
+parser.add_argument('--l2', action="store_true")
 
 def main(args):
     if args.compress:
@@ -23,9 +24,11 @@ def main(args):
     prob_dim = triples.shape[-1]
     logging.info('Training Index')
 
+
     faiss.normalize_L2(triples)
     quantiser = faiss.IndexHNSWFlat(prob_dim, 64) 
-    index = faiss.IndexIVFFlat(quantiser, prob_dim, args.k, faiss.METRIC_INNER_PRODUCT)
+    if not args.l2: index = index = faiss.IndexIVFFlat(quantiser, prob_dim, args.k)
+    else: faiss.IndexIVFFlat(quantiser, prob_dim, args.k, faiss.METRIC_L2)
 
     index.cp.min_points_per_centroid = 5
     index.quantizer_trains_alone = 2
