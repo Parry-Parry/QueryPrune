707a761240656ec02e6a12ec07a9ac0b13adeaf5
Andrew Parry <2393265p@student.gla.ac.uk>
2022-12-05 11:54:44 +0000

    wrong index int

--- queryreduce/models/markov.py
+++ queryreduce/models/markov.py
@@ -79,7 +79,7 @@ class Process:
     
     def _get_mean_batch(self, id: int) -> None:
         self.state_idx[0] = id
-        self.state_idx[1] = int(np.random.choice(self._expand_distance(self.triples[self.state_idx[i-1]])))
+        self.state_idx[1] = int(np.random.choice(self._expand_distance(self.triples[self.state_idx[0]])))
         for i in range(2, self.batch):
             vec = np.mean([self._expand(self.triples[self.state_idx[i-1]]), self._expand(self.triples[self.state_idx[i-2]])], axis=1)
             self.state_idx[i] = np.random.choice(self._distance(vec))
